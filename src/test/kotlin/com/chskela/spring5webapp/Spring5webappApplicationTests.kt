7aa2785207ce794d4b437ad1775cf5cd5a364f24
chskela <chskela@gmail.com>
2022-08-28 22:39:44 +0300

    Initial commit

---
+++ src/test/kotlin/com/chskela/spring5webapp/Spring5webappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chskela.spring5webapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
