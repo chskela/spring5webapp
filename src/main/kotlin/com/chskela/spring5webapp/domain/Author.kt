942b0cb832052c6918787db7f99a67066c844a9e
chskela <chskela@gmail.com>
2022-08-28 23:52:34 +0300

    Added entities Author and Book

---
+++ src/main/kotlin/com/chskela/spring5webapp/domain/Author.kt
@@ -0,0 +1,34 @@
+package com.chskela.spring5webapp.domain
+
+import org.hibernate.Hibernate
+import javax.persistence.*
+
+@Entity
+data class Author(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    val id: Long? = null,
+
+    val firstName: String,
+    val lastName: String,
+
+    @ManyToMany(mappedBy = "authors")
+    val books: Set<Book>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Author
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , firstName = $firstName , lastName = $lastName , books = $books )"
+    }
+
+}
---
+++ src/main/kotlin/com/chskela/spring5webapp/domain/Book.kt
@@ -0,0 +1,39 @@
+package com.chskela.spring5webapp.domain
+
+import org.hibernate.Hibernate
+import javax.persistence.*
+
+@Entity
+data class Book(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    val id: Long? = null,
+
+    val title: String,
+    val isbn: String,
+
+    @ManyToMany
+    @JoinTable(
+        name = "author_book",
+        joinColumns = [JoinColumn(name = "book_id")],
+        inverseJoinColumns = [JoinColumn(name = "author_id")]
+    )
+    val authors: Set<Author>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Book
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , title = $title , isbn = $isbn , authors = $authors)"
+    }
+
+}
